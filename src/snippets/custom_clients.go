c6b9dc93b950aaff203f6d4e64ea2402978c32fe
Jason Johnston <jasonjoh@users.noreply.github.com>
2023-12-19 11:07:00 -0500

    Updated proxy snippet to show how to use proxy for auth

--- src/snippets/custom_clients.go
+++ src/snippets/custom_clients.go
@@ -5,7 +5,12 @@ package snippets
 
 // <ImportSnippet>
 import (
+	"net/http"
+	"net/url"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	khttp "github.com/microsoft/kiota-http-go"
 	graph "github.com/microsoftgraph/msgraph-sdk-go"
 	graphcore "github.com/microsoftgraph/msgraph-sdk-go-core"
@@ -43,9 +48,26 @@ func NewGraphClientWithChaosHandler(credential azcore.TokenCredential, scopes []
 	return graphClient
 }
 
-func NewGraphClientWithProxy(credential azcore.TokenCredential, scopes []string) *graph.GraphServiceClient {
+func NewGraphClientWithProxy(scopes []string) *graph.GraphServiceClient {
 	// <ProxySnippet>
-	// tokenCredential is one of the credential classes from azidentity
+	proxyAddress := "http://proxy-url"
+	proxyUrl, _ := url.Parse(proxyAddress)
+
+	// Setup proxy for the token credential from azidentity
+	authClient := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+
+	credential, _ := azidentity.NewClientSecretCredential(
+		"CLIENT_ID", "TENANT_ID", "SECRET", &azidentity.ClientSecretCredentialOptions{
+			// Pass the proxied client to the credential
+			ClientOptions: policy.ClientOptions{
+				Transport: authClient,
+			},
+		})
+
 	// scopes is an array of permission scope strings
 	authProvider, _ := authentication.NewAzureIdentityAuthenticationProviderWithScopes(credential, scopes)
 
@@ -54,10 +76,10 @@ func NewGraphClientWithProxy(credential azcore.TokenCredential, scopes []string)
 	defaultMiddleWare := graphcore.GetDefaultMiddlewaresWithOptions(&defaultClientOptions)
 
 	// Create an HTTP client with the middleware
-	httpClient, _ := khttp.GetClientWithProxySettings("http://proxy-url", defaultMiddleWare...)
+	httpClient, _ := khttp.GetClientWithProxySettings(proxyAddress, defaultMiddleWare...)
 	// For authenticated proxy, use
 	// khttp.GetClientWithAuthenticatedProxySettings(
-	//     "http://proxy-url", "user", "password", defaultMiddleWare...)
+	//     proxyAddress, "user", "password", defaultMiddleWare...)
 
 	// Create the adapter
 	// Passing nil values causes the adapter to use default implementations
